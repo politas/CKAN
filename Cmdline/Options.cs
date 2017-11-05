65205ee8b90f45b9a78100a4e9eb3a23f15f8d7a
Myk <politas@gmail.com>
2017-11-05 14:56:08 +1100

    Debugged CmdLine changes

--- Cmdline/Options.cs
+++ Cmdline/Options.cs
@@ -73,6 +73,9 @@ namespace CKAN.CmdLine
         [VerbOption("repair", HelpText = "Attempt various automatic repairs")]
         public SubCommandOptions Repair { get; set; }
 
+        [VerbOption("replace", HelpText = "Replace list of replaceable mods")]
+        public ReplaceOptions Replace { get; set; }
+
         [VerbOption("repo", HelpText = "Manage CKAN repositories")]
         public SubCommandOptions KSP { get; set; }
 
